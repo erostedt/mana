e2975fbb0bddfd3adea005f23db216c6de63f839
Eric Rostedt <ericrostedt@erics-air.mynet>
2023-07-02 17:35:55 +0200

    add test to trie

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"os"
-	"strings"
-)
-
 func main() {
-	data, err := os.ReadFile("animals.txt")
 
-	if err != nil {
-		fmt.Print("Could not read file.")
-		os.Exit(1)
-	}
-
-	trie := NewTrie(5)
-	animals := strings.Split(string(data), "\n")
-	for _, animal := range animals {
-		trie.Insert(animal)
-	}
-
-	fmt.Println(trie.Autocomplete("Pe", 3))
 }
---
+++ trie_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestTrieComplete(t *testing.T) {
+	data, err := os.ReadFile("animals.txt")
+
+	if err != nil {
+		t.Error("Could not read file.")
+	}
+
+	trie := NewTrie(5)
+	animals := strings.Split(string(data), "\n")
+	for _, animal := range animals {
+		trie.Insert(animal)
+	}
+
+	suggestions := trie.Autocomplete("Pe", 2)
+
+	if suggestions[0] != "Penguin" {
+		t.Errorf("First suggestion should have been Penguin, was %s", suggestions[0])
+	}
+
+	if suggestions[1] != "Peacock" {
+		t.Errorf("Second suggestion should have been Peacock, was %s", suggestions[1])
+	}
+}
